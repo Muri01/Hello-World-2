cb45f6d210866c73e48274d366a4040eed726925
Murilo De Sousa Gonçalves <40380133+Muri01@users.noreply.github.com>
2025-06-09 19:04:19 -0300

    criar lista e mostrar na página 1 elemento dela

--- main.js
+++ main.js
@@ -1,8 +1,37 @@
 function inserirNome(){
    let nomeUsuario = prompt("Qual o seu nome");
    let elemento = document.querySelector("#nome-usuario");
-   console.log(elemento);
    elemento.textContent = nomeUsuario;
 }
 
 inserirNome();
+
+//Lista de linguagens escritas:
+const linguagens = [, "JavaScript", "python", "C"]
+
+console.log(linguagens[0]) // “JavaScript”
+console.log(linguagens[1]) // “Python”
+console.log(linguagens[2]) // “C”
+
+
+// Adicionar elemento da lista na página com DOM.
+const item = document.querySelector("#lista");  //seleciona elemento com id "lista" e guarda na variável "item"
+item.textContent = linguagens[0]; // altera o elemento da variável "item" para o valor 0 da lista "linguagens"
+
+
+
+// for (const indice of linguagens) {
+//   const item = document.createElement("li");
+//   item.textContent = indice;
+//   lista.appendChild(item);
+// }
+
+
+
+// Objetos
+const html = {
+   nome: "html5",
+   anoCriacao: 1991,
+   usadaNoComponente: true,
+   segmentoUsada: ["1ªsérie","2ªsérie","3ªsérie"]
+}
